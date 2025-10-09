9b11c0c3fb31118d80d2677f248f760481f5c222
ShonP40 <13995143+ShonP40@users.noreply.github.com>
2025-10-09 17:12:30 +0300

    Added timeout to the `batterydata` commands
    To avoid lockups

--- script.py
+++ script.py
@@ -1,14 +1,41 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import subprocess
 import simplejson as json
+import time
 
 def get_percentage():
-    return (subprocess.Popen(['batterydata | grep "Current Capacity" | awk \'{print $4}\''], shell=True, stdout=subprocess.PIPE).communicate()[0]).strip()
+    return run_cmd('batterydata | grep "Current Capacity" | awk \'{print $4}\'', timeout=5).strip()
 
 def get_charging_status():
-    status = (subprocess.Popen(['batterydata | grep "Is Charging" | awk \'{print $4}\''], shell=True, stdout=subprocess.PIPE).communicate()[0]).strip()
-
+    status = run_cmd('batterydata | grep "Is Charging" | awk \'{print $4}\'', timeout=5).strip()
     return "Charging" if status == '1' else "NotCharging"
+
+def run_cmd(cmd, timeout):
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    start = time.time()
+
+    while True:
+        ret = proc.poll()
+        if ret is not None:
+            out, _ = proc.communicate()
+            try:
+                return out
+            except OSError:
+                return ''
+        if time.time() - start > timeout:
+            try:
+                proc.terminate()
+            except (OSError, AttributeError):
+                pass
+            time.sleep(0.1)
+            if proc.poll() is None:
+                try:
+                    proc.kill()
+                except (OSError, AttributeError):
+                    pass
+            out, _ = proc.communicate()
+            return out or ''
+        time.sleep(0.1)
 
 class serverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
