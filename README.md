58916f0534c80c065fedd526c571a7144b45ea09
ShonP40 <13995143+ShonP40@users.noreply.github.com>
2026-02-16 19:45:19 +0200

    Added support for 64-bit devices

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ A REST API for reporting the battery percentage of jailbroken iOS devices
 ## Installation
 
 ### Requirements
-- Jailbroken iOS device (only 32-bit devices are supported at the moment)
+- Jailbroken iOS device
 - Terminal/SSH access to your iOS device
 - (Recommended) A tweak that keeps Wi-Fi on even when the device is locked (like `iNoSleep`)
 - Git CLI (`apt-get install git`) on your iOS device
-- Python 2.5.1 (`apt-get install python`) on your iOS device
 - Awk (`apt-get install gawk`) on your iOS device
 - Screen (`apt-get install screen`) on your iOS device
+- Python 2.5.1 (`apt-get install python`) (32-bit) / Python 3.9.9 (`apt-get install python3`) (64-bit) on your iOS device
 
-## Setup
+## Setup (32-bit)
 1. Meet the requirements
 2. Follow the instructions [here](https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent) to generate an SSH key on your iOS device to be able to access GitHub
 3. Run `cat /path/to/<your key>.pub` and copy its contents to this [GitHub](https://github.com/settings/ssh/new) page
@@ -21,21 +21,29 @@ A REST API for reporting the battery percentage of jailbroken iOS devices
 6. cd into the `simplejson` directory
 7. Install the `simplejson` package using `python setup.py install`
 8. cd into the `iOS-REST-Battery` directory
-9. Install `batterydata-arm32.deb` using `dpkg -i batterydata-arm32.deb`
-10. Start the script by running `python script.py` to test it out
+9. Install `batterydata` using `dpkg -i batterydata-arm32.deb`
+10. Start the script by running `python battery-arm32.py` to test it out
 11. Press `Ctrl + C` to stop the script
+
+## Setup (64-bit)
+1. Meet the requirements
+2. Clone this repository to your iOS device (`git clone https://github.com/ShonP40/iOS-REST-Battery`)
+3. cd into the `iOS-REST-Battery` directory
+4. Install `batterydata` using `dpkg -i batterydata-arm64-rootfull.deb` or `dpkg -i batterydata-arm64-rootless.deb` depending on your setup
+5. Start the script by running `python3 battery-arm64.py` to test it out
+6. Press `Ctrl + C` to stop the script
 
 ## Running (You will need to do this every time you reboot your device)
 1. cd into the `iOS-REST-Battery` directory
 2. Create a new screen session by running `screen -S battery`
-3. Start the script by running `python script.py`
+3. Start the script
 4. Detach from the screen session by pressing `Ctrl + A + D`
 
 ## Updating
 1. Reattach to the screen session by running `screen -r battery`
 2. Press `Ctrl + C` to stop the script
 3. Run `git pull`
-4. Start the script again by running `python script.py`
+4. Start the script again
 5. Detach from the screen session by pressing `Ctrl + A + D`
 
 ## Usage
---
+++ battery-arm64.py
@@ -0,0 +1,128 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import subprocess
+import json
+import time
+import socket
+import errno
+from socketserver import ThreadingMixIn
+
+def get_percentage():
+    return run_cmd('batterydata | grep "Current Capacity" | awk \'{print $4}\'').strip()
+
+def get_charging_status():
+    status = run_cmd('batterydata | grep "Is Charging" | awk \'{print $4}\'').strip()
+    return "Charging" if status == '1' else "NotCharging"
+
+def run_cmd(cmd):
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    start = time.time()
+
+    while True:
+        ret = proc.poll()
+        if ret is not None:
+            out, _ = proc.communicate()
+            try:
+                return out.decode('utf-8')
+            except OSError:
+                return ''
+        if time.time() - start > 5:
+            try:
+                proc.terminate()
+            except (OSError, AttributeError):
+                pass
+            time.sleep(0.1)
+            if proc.poll() is None:
+                try:
+                    proc.kill()
+                except (OSError, AttributeError):
+                    pass
+            out, _ = proc.communicate()
+            return out.decode('utf-8') if out else ''
+        time.sleep(0.1)
+
+class serverHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path == '/status':
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                body = json.dumps({'Battery': get_percentage(), 'Battery status': get_charging_status()})
+                safe_write(self.wfile, body)
+            else:
+                self.send_error(404)
+        except KeyboardInterrupt:
+            return
+        except socket.error as e:
+            try:
+                err_no = e.errno
+            except AttributeError:
+                try:
+                    err_no = e[0]
+                except Exception:
+                    err_no = None
+            if err_no == errno.EPIPE:
+                return
+            return
+        except IOError as e:
+            try:
+                err_no = e.errno
+            except AttributeError:
+                try:
+                    err_no = e[0]
+                except Exception:
+                    err_no = None
+            if err_no == errno.EPIPE:
+                return
+            return
+
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+    daemon_threads = True
+
+
+def safe_write(wfile, data):
+    """Write to wfile and ignore EPIPE/Broken pipe errors (client disconnected).
+
+    wfile is the socket-like file object (self.wfile). data should be a str
+    (text).
+    """
+    try:
+        wfile.write(data.encode('utf-8'))
+    except socket.error as e:
+        err_no = None
+        try:
+            err_no = e.errno
+        except Exception:
+            try:
+                idx = e[0]
+                if isinstance(idx, int):
+                    err_no = idx
+            except Exception:
+                err_no = None
+        if err_no == errno.EPIPE:
+            return
+        raise
+
+def main():
+    server = ThreadedHTTPServer(('0.0.0.0', 8000), serverHandler)
+    print('Starting server, use <Ctrl-C> to stop')
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print('Shutting down server')
+        try:
+            server.shutdown()
+        except Exception:
+            try:
+                server.socket.close()
+            except Exception:
+                pass
+        try:
+            server.server_close()
+        except Exception:
+            pass
+
+if __name__ == '__main__':
+    main()
